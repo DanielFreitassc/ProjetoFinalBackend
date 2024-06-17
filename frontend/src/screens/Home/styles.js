f96d438c639f7b6178df9cbcd01aadc39669051a
DanielFreitassc <danielfreitassc@gmail.com>
2024-06-17 18:36:43 -0300

    Feat: firulas

--- frontend/src/screens/Home/styles.js
+++ frontend/src/screens/Home/styles.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
 export const ContainerHome = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 100%;
     height: 100vh;
+    background-color: black;
+`
+export const ImageContent = styled.img`
+
+    height: 100%;
+    width: 100vh;
 `
